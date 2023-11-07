ccfd35337e7f2dc4bbea2062a44e46d32b308f65
Vibilishwer <145904866+Vibilishwer@users.noreply.github.com>
2023-11-07 14:50:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,25 +5,25 @@ import pymongo
 import mysql.connector
 
 #=======================GOOGLE API KEY=================================
-api_key = 'AIzaSyCR3vclDR-oPLcz_FQmWjV9Ap7qJl9P6v4'
+api_key = 'copy your API from youtube'
 youtube = build('youtube', 'v3', developerKey=api_key)
 channel_id = "UCmyKnNRH0wH-r8I-ceP-dsg"
 
 
 #======================MONGODB CONNECTION==============================
-client = pymongo.MongoClient("mongodb+srv://vibilishwer:Guvi2023@cluster0.eefu6ud.mongodb.net/")
+client = pymongo.MongoClient("mongodb+srv://YourUserName:Password@cluster0.eefu6ud.mongodb.net/")
 database = client["youtube"]
 collection = database["channel_information"]
 
 #=====================SQLALCHEMY FOR SEND AS DATAFRAME================
-engine = create_engine('mysql+mysqlconnector://root:root@localhost/mydatabase')
+engine = create_engine('mysql+mysqlconnector://root:YOUR PASSWORD@localhost/mydatabase')
 
 
 #=====================MYSQL - PYTHON CONNECTION=======================
 mydb = mysql.connector.connect(
   host="localhost",
-  user="root",
-  password ="root",
+  user="ROOT",
+  password ="YOUR PASSWORD",
   database="mydatabase"
 )
 mycursor = mydb.cursor()
